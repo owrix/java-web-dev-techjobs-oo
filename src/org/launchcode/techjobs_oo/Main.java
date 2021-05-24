f1c5da068491d39bf3ab139bc96efb3ff29c820c
Your Name <you@example.com>
2021-05-24 15:57:13 -0500

    extended jobfield to Location CoreComp and PositionType

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType(
                 "Front-end developer"), new CoreCompetency("JavaScript"));
         Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"),
-                new CoreCompetency("Taste"));
+                new CoreCompetency("Tasting Ability"));
 
         ArrayList<Job> jobs = new ArrayList<>();
         jobs.add(job1);
